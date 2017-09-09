a2d12e5e4904716ac8cc5d6ce2fea41063fc7316
Jasonellen <nannan@jasondeMacBook-Pro.local>
2017-09-09 13:07:40 +0800

    add .

---
+++ js/common.js
@@ -0,0 +1,16 @@
+(function(global){
+	function remChange(){
+		var w=document.documentElement.clientWidth;
+		if(w>620){
+			w=620;
+		} 
+		if(w<320){
+			w=320;
+		}
+		document.documentElement.style.fontSize=w/375*100+'px'
+	}
+	remChange();
+	global.addEventListener('resize',remChange,false)
+})(window)
+
+
